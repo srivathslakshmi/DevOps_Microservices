519e4b1fec1b19635c574c22e959637eebfcf679
srivathslakshmi <ec2-user@ip-172-31-64-242.ec2.internal>
2022-08-18 14:39:58 +0000

    fix wikipedia

--- wikipedia2022/hello_world/app.py
+++ wikipedia2022/hello_world/app.py
@@ -11,21 +11,38 @@ def lambda_handler(event, context):
         :return: a response that contains the first sentence of a wikipedia page,
             the response is JSON formatted.'''
     
-    ## TO DO: Check that the request has some input body and save it
+    # check that the request has some input body
     if 'body' in event:
-        body = None
+        event = json.loads(event["body"])
     
-    ## TO DO: Get the wikipedia "entity" from the body of the request
-    entity = None
-    res = wikipedia.summary(entity, sentences=1) # first sentence, result
+    # get the wikipedia "entity" from the body of the request
+    entity = event["entity"]
+    # res = wikipedia.summary(entity, sentences=1) # first sentence, result
+    BAD_REQUEST_STATUS = 400
+    ALL_GOOD_STATUS = 200 
+    # Exception handling
+    try:
+        res = wikipedia.summary(entity, sentences=1) # first sentence, result
+        statusCode = ALL_GOOD_STATUS
+    except wikipedia.exceptions.PageError:
+        res= "\nThis word does not exist!\n"
+        statusCode = BAD_REQUEST_STATUS
+    except wikipedia.exceptions.DisambiguationError: 
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nThere are multiple references to this word!\n"
+    except:
+        statusCode = BAD_REQUEST_STATUS
+        res = "\nSorry, Cannot Handle this request!\n"
 
     # print statements
     print(f"context: {context}, event: {event}")
     print(f"Response from wikipedia API: {res}")
     
-    ## TO DO: Format the response as JSON and return the result
+    # format the response as JSON and return the result
     response = {
-        ## your code here
+        "statusCode": statusCode, 
+        "headers": { "Content-type": "application/json" },
+        "body": json.dumps({"message": res})
     }
     
     return response
